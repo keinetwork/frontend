cf06efd73bcdeab3ea7b2cfd96b374b3b8ec7673
김영석 <keinetwork@gmail.com>
2023-02-18 22:44:34 +0900

    05. Getter, Setter

--- vue3-computed/src/App.vue
+++ vue3-computed/src/App.vue
@@ -1,26 +1,48 @@
 <template>
+  <button @click="add">
+    ADD
+  </button>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reverseMessage() }}</h1>
-  <h1>{{ msg.split('').reverse().join('') }}</h1>
-  <h1>{{ msg.split('').reverse().join('') }}</h1>
+  <h1>{{ msg.split("").reverse().join("") }}</h1>
+  <h1>{{ msg.split("").reverse().join("") }}</h1>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-      msg: 'Hello Computed!'
+      msg: "Hello Computed!",
     }
   },
   computed: {
-    reversedMessage() { // 캐싱
-      return this.msg.split('').reverse().join('')
+    reverseGetSet: {
+      get() {
+        return this.msg.split("").reverse().join("")
+      },
+      set(value) {
+        this.msg = value
+        console.log(value)
+      }
+    },
+    reversedMessage() {
+      // 캐싱
+      return this.msg.split("").reverse().join("");
     }
   },
   methods: {
+    // Getter
     reverseMessage() {
-      return this.msg.split('').reverse().join('')
-    }
+      return this.msg.split("").reverse().join("");
+    },
+    add() {
+      // this.reversedMessage += "!?"; // 읽기 전용
+      // Setter
+      // this.msg += '!?'
+      this.reverseGetSet += "!?"; // Get, Set
+
+    },
   }
-}
+};
 </script>
