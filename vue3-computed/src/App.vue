8fe075092dfa2b3f1900ae1fd0b461548c526806
김영석 <keinetwork@gmail.com>
2023-02-18 22:48:59 +0900

    06

--- vue3-computed/src/App.vue
+++ vue3-computed/src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <button @click="add">
-    ADD
-  </button>
+  <h1>{{ msg }}</h1>
   <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reverseMessage() }}</h1>
-  <h1>{{ msg.split("").reverse().join("") }}</h1>
-  <h1>{{ msg.split("").reverse().join("") }}</h1>
 </template>
 
 <script>
@@ -13,36 +8,18 @@
 export default {
   data() {
     return {
-      msg: "Hello Computed!",
+      msg: "Hello?",
     }
   },
   computed: {
-    reverseGetSet: {
-      get() {
-        return this.msg.split("").reverse().join("")
-      },
-      set(value) {
-        this.msg = value
-        console.log(value)
-      }
-    },
     reversedMessage() {
-      // 캐싱
       return this.msg.split("").reverse().join("");
     }
   },
   methods: {
-    // Getter
-    reverseMessage() {
-      return this.msg.split("").reverse().join("");
-    },
-    add() {
-      // this.reversedMessage += "!?"; // 읽기 전용
-      // Setter
-      // this.msg += '!?'
-      this.reverseGetSet += "!?"; // Get, Set
-
-    },
+    changeMessage() {
+      return this.msg="Good!"
+    }
   }
-};
+}
 </script>
