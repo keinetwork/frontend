b80ec1c7ec9073989c5b465d9129eef1d8801a9b
김영석 <keinetwork@gmail.com>
2023-02-21 19:22:09 +0900

    13-result

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <input type="text" />
+  <h1>{{ msg }}</h1>
+  <input 
+    type="text" 
+    :value="msg" 
+    @input="msg= $event.target.value"/>
+    <h1>{{  checked }}</h1>
+    <input 
+      type="checkbox"
+      v-model="checked" />
 </template>
 
 <script> 
 export default {
   data() {
     return {
-      
+      msg: 'Hello world!',
+      checked: false
+    }
+  },
+  methods: {
+    handler(event) {
+      console.log(event.target.value)
     }
   }
 }
