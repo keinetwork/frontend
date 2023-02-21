cb470515d5cb438c7771589b10ad9a739df0f7c1
김영석 <keinetwork@gmail.com>
2023-02-21 16:57:59 +0900

    10

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -2,43 +2,13 @@
   <button @click="handler">
     Click me!
   </button>
-  <ul>
-    <li
-      v-for="(f, i) in fruits"
-      :key="f">
-      {{ f }}-{{ i +1}}
-    </li>
-  </ul>
-  <ul>
-    <li
-      v-for="{id, name} in newFruits"
-      :key="id">
-      {{ name }}-{{ id }}
-    </li>
-  </ul>
 </template>
 
 <script> 
-import shortid from 'shortid'
-
 export default {
-  data() {
-    return {
-      fruits: ['Apple', 'Banana', 'Cherry'],
-
-    }
-  },
-  computed: {
-    newFruits() {
-      return this.fruits.map(fruit=> ({
-          id: shortid.generate(),
-          name: fruit
-      }))
-    }
-  },
   methods: {
     handler() {
-      this.fruits.push('Orange')
+      console.log(event)
     }
   }
 }
