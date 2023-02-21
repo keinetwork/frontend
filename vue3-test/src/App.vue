6f083f8d35a9066bf2c8faf635b3c7719805aec9
김영석 <keinetwork@gmail.com>
2023-02-21 16:13:58 +0900

    09

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,35 +1,19 @@
 <template>
-  <button @click="handler">Click me!</button>
-  <h1 v-if="isShow">Hello?!</h1>
-  <h1 v-else-if="count > 3">
-    Count > 3
-  </h1>
-  <h1 v-else>
-    Good~
-  </h1>
-  <template v-if="isShow">
-    <h1>Title</h1>
-    <p>Paragraph 1</p>
-    <p>Paragraph 2</p> 
-  </template> 
-  <h1 v-show="isShow">
-    Hello?!
-  </h1>
+  <ul>
+    <li
+      v-for="fruit in fruits"
+      :key="fruit">
+      {{ fruit }}
+    </li>
+  </ul>
 </template>
 
 <script> 
 export default {
   data() {
     return {
-      isShow: false,
-      count: 0
-    };
-  },
-  methods: {
-    handler() {
-      this.isShow = !this.isShow;
-      this.count += 1;
+      fruits: ['Apple', 'Banana', 'Cherry']
     }
-  },
-};
+  }
+}
 </script>
