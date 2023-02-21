a666d4c3dadae84146fce4f30e7a6c2d4ad93de7
김영석 <keinetwork@gmail.com>
2023-02-21 17:49:25 +0900

    11

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,27 +1,17 @@
 <template>
-  <button @click="handler('hi', $event)">
-    Click 1
-  </button>
-  <button @click="handler('what', $event)">
-    Click 2
-  </button>
-  <button @click="handlerA(), handlerB()">
-    Click me!
-  </button>
+  <a
+    href="https://naver.com"
+    target="_blank"
+    @click="handler">
+    NAVER
+  </a>
 </template>
 
 <script> 
 export default {
   methods: {
-    handler(msg, event) {
-      console.log(msg)
-      console.log(event)
-    },
-    handlerA() {
-      console.log('A')
-    },
-    handlerB() {
-      console.log('B')
+    handler() {
+      console.log('ABC!')
     }
   }
 }
