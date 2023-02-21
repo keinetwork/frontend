700674f0ed471022812c56485b0977d727c0da4a
김영석 <keinetwork@gmail.com>
2023-02-21 23:03:14 +0900

    20. 컴포넌트 - Refs

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,28 +1,11 @@
 <template>
-  <button @click="message= 'Good?'">
-    Click!
-  </button>
-  <h1>App: {{ message }}</h1>
-  <Parent />
+  <h1>
+    Hello world!
+  </h1>
 </template>
 
 <script> 
-import Parent from '~/components/Parent'
-import { computed } from 'vue'
 
 export default {
-  components: {
-    Parent
-  },
-  data() {
-    return {
-      message: 'Hello world!'
-    }
-  },
-  provide() {
-    return {
-      msg: computed(() => this.message)
-    }
-  }
 }
 </script>
