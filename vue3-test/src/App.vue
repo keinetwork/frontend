6941c2b263538221effb77af233d219de1f8278a
김영석 <keinetwork@gmail.com>
2023-02-21 22:45:21 +0900

    19. 컴포넌트 -Provide, Inject

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <MyBtn>
-    <template #text>
-      <span>Banana</span>
-    </template>
-    <template #icon>
-      <span>(B)</span>
-    </template>
-  </MyBtn>
+  <Parent :msg="message" />
 </template>
 
 <script> 
-import MyBtn from '~/components/MyBtn'
+import Parent from '~/components/Parent'
 
 export default {
   components: {
-    MyBtn
+    Parent
+  },
+  data() {
+    return {
+      message: 'Hello world!'
+    }
   }
 }
 </script>
