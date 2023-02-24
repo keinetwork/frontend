d9fee3d270b7aceadd0bbe40fd8528359a0f241a
김영석 <keinetwork@gmail.com>
2023-02-24 21:17:50 +0900

    12. 영화 검색 추가 요청

--- vue3-movie-app/src/store/movie.js
+++ vue3-movie-app/src/store/movie.js
@@ -27,7 +27,7 @@ export default {
     // 비동기
     actions: {
         
-        async searchMovies({ commit }, payload) {
+        async searchMovies({ commit, state }, payload) {
             const { title, type, number, year} = payload
             const OMDB_API_KEY = '7035c60c'
             const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
@@ -35,6 +35,22 @@ export default {
             commit('updateState', {
                 movies: Search
             })
+            console.log(totalResults)
+            console.log(typeof totalResults)
+
+            const total = parseInt(totalResults, 10)
+            const pageLength = Math.ceil(total/10)
+            // 추가 요청!
+            if ( pageLength > 1) {
+                for(let page = 2; page <= pageLength; page += 1) {
+                    if( page > number / 10 ) break                       
+                    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+                    const { Search } = res.data
+                    commit('updateState', {
+                        movies: [...state.movies, ...Search]
+                    })
+                }
+            }
         }
     }
 }
