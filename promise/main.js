61afc456a50476847d262ecfa331d8b45b48e280
김영석 <keinetwork@gmail.com>
2023-03-01 21:21:36 +0900

    15. 비동기 -예외처리(then, catch, finally)

--- promise/main.js
+++ promise/main.js
@@ -1,90 +1,112 @@
-function a(callback) {
-    const str = 'Hello A'
-    setTimeout(()=> {
-        console.log('A')
-        callback(str)
-    }, 1000)
-}
-
-
-function c(callback) {
-    setTimeout(()=> {
-        console.log('C')
-        callback()
-    }, 1000)
-}
-
-
-function d(callback) {
-    setTimeout(()=> {
-        console.log('D')
-        callback()
-    }, 1000)
-}
-
-function b(callback) {
-    setTimeout(()=> {
-        console.log('B')
-        callback()
-    }, 1000)
-}
-
-// a(function () {
-//     b( function() {
-//         c(function() {
-//             d(function(){
-//                 console.log('Done!')
-//             } )
-//         })
-//     })
-// })
-
-function aa() {
-    // promise: 약속의 객ㅔ!
-    return new Promise(function (resolve) {
-        setTimeout(function() {
+function a() {
+    return new Promise((resolve)=> {
+        setTimeout(()=>{
             console.log('A')
-            resolve('Hello A')
+            resolve()
         }, 1000)
     })
 }
 
-function bb() {
-    // promise: 약속의 객ㅔ!
-    return new Promise(function (resolve) {
-        setTimeout(function() {
+function b() {
+    return new Promise((resolve)=> {
+        setTimeout(()=>{
             console.log('B')
-            resolve('Hello B')
+            resolve()
         }, 1000)
     })
 }
-function cc() {
-    // promise: 약속의 객ㅔ!
-    return new Promise(function (resolve) {
-        setTimeout(function() {
+
+function c() {
+    return new Promise((resolve)=> {
+        setTimeout(()=>{
             console.log('C')
-            resolve('Hello C')
+            resolve()
         }, 1000)
     })
 }
 
-function dd() {
-    // promise: 약속의 객ㅔ!
-    return new Promise(function (resolve) {
-        setTimeout(function() {
+function d() {
+    return new Promise((resolve)=> {
+        setTimeout(()=>{
             console.log('D')
-            resolve('Hello D')
+            resolve()
         }, 1000)
     })
 }
 
-async function test() {
-    const h1 = await aa()
-    const h2 = await bb()
-    const h3 = await cc()
-    const h4= await dd()
-    console.log('Done!')
-    console.log(h1, h2, h3, h4)
+function test() {
+    // const promise = a()
+    // promise.then(()=>{
+    //     console.log('B')
+
+    // })
+
+    a().then(()=>{
+        b().then(()=>{
+            c().then(()=>{
+                d().then(()=>{
+                    console.log('Done!')
+                })
+            })
+        })
+    })
+
+    a().then(()=>{
+        return b()
+    }).then(()=>{
+        return c()
+    }).then(()=>{
+        return d()
+    }).then(()=>{
+        console.log('Done!')
+    })
+
+    a()
+        .then(()=> b())
+        .then(()=> c())
+        .then(()=> d())
+        .then(()=> {
+            console.log('Done!')
+        })
 }
 
-test()
+// test()
+
+function aa(number) {
+    return new Promise((resolve, reject)=> {
+        if( number > 4) {
+            reject()
+            return 
+        }
+        setTimeout(()=>{
+            console.log('A')
+            resolve()
+        }, 1000)
+    })
+}
+
+function test2() {
+    aa(4)
+        .then(()=>{
+            console.log('Resolve!')
+        })
+        .catch(()=>{
+            console.log('Reject!')
+        })
+        .finally(()=>{
+            console.log('Done!')
+        })
+}
+
+async function test3(){
+    try{     
+        await aa(8)
+        console.log('Resolve!')   
+    } catch(error) {
+        console.log('Reject!')
+    } finally {
+        console.log('Done!')
+    }
+}
+
+test3()
