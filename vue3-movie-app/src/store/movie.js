b86a5ea94ba7bef53c50afc8820f30c608865c47
김영석 <keinetwork@gmail.com>
2023-06-16 23:44:27 +0900

    17-_fetchMovie

--- vue3-movie-app/src/store/movie.js
+++ vue3-movie-app/src/store/movie.js
@@ -29,9 +29,10 @@ export default {
     actions: {
         
         async searchMovies({ commit, state }, payload) {
-            const { title, type, number, year} = payload
-            const OMDB_API_KEY = '7035c60c'
-            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+            const res = await _fetchMovie({
+                ...payload,
+                page: 1
+            })
             const { Search, totalResults } = res.data
             commit('updateState', {
                 movies: _uniqBy(Search, 'imdbID')
@@ -44,8 +45,11 @@ export default {
             // 추가 요청!
             if ( pageLength > 1) {
                 for(let page = 2; page <= pageLength; page += 1) {
-                    if( page > number / 10 ) break                       
-                    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+                    if( page > payload.number / 10 ) break                       
+                    const res = await _fetchMovie({
+                        ...payload,
+                        page
+                    })
                     const { Search } = res.data
                     commit('updateState', {
                         movies: [
@@ -57,4 +61,20 @@ export default {
             }
         }
     }
+}
+
+function _fetchMovie(payload) {
+    const { title, type, year, page } = payload
+    const OMDB_API_KEY = '7035c60c'
+    const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+    
+    return new Promise((resolve, reject)=> {
+        axios.get(url)
+            .then(res=>{
+                resolve(res)
+            })
+            .catch(err => {
+                reject(err.message)
+            })
+    })
 }
