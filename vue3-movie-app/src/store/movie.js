f8b488746c47a918679bf07f469aa210bc5343f8
김영석 <keinetwork@gmail.com>
2023-06-17 11:54:57 +0900

    17. result

--- vue3-movie-app/src/store/movie.js
+++ vue3-movie-app/src/store/movie.js
@@ -28,36 +28,43 @@ export default {
     // 비동기
     actions: {
         
-        async searchMovies({ commit, state }, payload) {
-            const res = await _fetchMovie({
-                ...payload,
-                page: 1
-            })
-            const { Search, totalResults } = res.data
-            commit('updateState', {
-                movies: _uniqBy(Search, 'imdbID')
-            })
-            console.log(totalResults)
-            console.log(typeof totalResults)
-
-            const total = parseInt(totalResults, 10)
-            const pageLength = Math.ceil(total/10)
-            // 추가 요청!
-            if ( pageLength > 1) {
-                for(let page = 2; page <= pageLength; page += 1) {
-                    if( page > payload.number / 10 ) break                       
-                    const res = await _fetchMovie({
-                        ...payload,
-                        page
-                    })
-                    const { Search } = res.data
-                    commit('updateState', {
-                        movies: [
-                            ...state.movies, 
-                            ..._uniqBy(Search, 'imdbID')
-                        ]
-                    })
+        async searchMovies({ state, commit }, payload) {
+            try {
+                const res = await _fetchMovie({
+                    ...payload,
+                    page: 1
+                })
+                const { Search, totalResults } = res.data
+                commit('updateState', {
+                    movies: _uniqBy(Search, 'imdbID')
+                })
+                console.log(totalResults)
+                console.log(typeof totalResults)
+    
+                const total = parseInt(totalResults, 10)
+                const pageLength = Math.ceil(total/10)
+                // 추가 요청!
+                if ( pageLength > 1) {
+                    for(let page = 2; page <= pageLength; page += 1) {
+                        if( page > payload.number / 10 ) break                       
+                        const res = await _fetchMovie({
+                            ...payload,
+                            page
+                        })
+                        const { Search } = res.data
+                        commit('updateState', {
+                            movies: [
+                                ...state.movies, 
+                                ..._uniqBy(Search, 'imdbID')
+                            ]
+                        })
+                    }
                 }
+            } catch (message) {
+                commit('updateState', {
+                    movies: [],
+                    message
+                })
             }
         }
     }
@@ -71,6 +78,9 @@ function _fetchMovie(payload) {
     return new Promise((resolve, reject)=> {
         axios.get(url)
             .then(res=>{
+                if (res.data.Error) {
+                    reject(res.data.Error)
+                }
                 resolve(res)
             })
             .catch(err => {
